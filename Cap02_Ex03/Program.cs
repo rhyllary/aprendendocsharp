c03f8a3c30733a810bb9fa507deed101da117cbf
RHYLLARY VITORIA GIOPATTO <rhyllary.vgiopatto@senacsp.edu.br>
2024-08-06 17:07:01 -0300

    teste 3/1

---
+++ Cap02_Ex03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float HT, VH, PD, SB, TD, SL;
+
+            Console.Write("horas trabalhadas...............:");
+            HT = float.Parse(Console.ReadLine());
+            Console.Write("valor da hora...................:");
+            VH = float.Parse(Console.ReadLine());
+            Console.Write("valor do oercentual de desconto.:");
+            PD = float.Parse(Console.ReadLine());
+
+            SB = HT * VH;
+            TD = (PD / 100) * SB;
+            SL = SB - TD;
+
+            Console.WriteLine();
+            Console.Write("Salario liquido.................:");
+            Console.WriteLine(SL.ToString("##,##0.00"));
+
+            Console.WriteLine();
+            Console.Write("tecle <enter> para encerrar... ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cap03_Ex01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap03_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R;
+
+            Console.Write("entre o valor <A>: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("entre o valor <B>: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine("resultado = {0}", R);
+
+            Console.WriteLine();
+            Console.Write("tecle <enter> para encerrar.... ");
+            Console.ReadLine();
+        }
+    }
+}
