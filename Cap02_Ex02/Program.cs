2b6d08aa839f9056c458e111a88c566e693fb77a
RHYLLARY VITORIA GIOPATTO <rhyllary.vgiopatto@senacsp.edu.br>
2024-08-06 17:07:01 -0300

    teste 2

---
+++ Cap02_Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R;
+            Console.Write("entre o 1o. valor: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("entre o 2o. valor: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            Console.WriteLine("resultado = " + R);
+
+            Console.WriteLine();
+            Console.Write("tecle <enter> para encerrar... ");
+            Console.ReadLine();
+        }
+    }
+}
